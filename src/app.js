fb0f4ee95c179a1eaf470c3fa5432c29de933826
Karoll Guzmán <karolldguzman@gmail.com>
2024-11-19 19:45:57 +0000

    lineamiento de código

--- src/app.js
+++ src/app.js
@@ -30,19 +30,11 @@ document.addEventListener("DOMContentLoaded", function() {
       "hace una semana"
     ];
 
-    let randomWho = Math.floor(Math.random() * who.length);
-    let randomAction = Math.floor(Math.random() * action.length);
-    let randomWhat = Math.floor(Math.random() * what.length);
-    let randomWhen = Math.floor(Math.random() * when.length);
+    let WhoRandomIndex = Math.floor(Math.random() * who.length);
+    let ActionRandomIndex = Math.floor(Math.random() * action.length);
+    let WhatRandomIndex = Math.floor(Math.random() * what.length);
+    let WhenRandomIndex = Math.floor(Math.random() * when.length);
 
-    return (
-      who[randomWho] +
-      " " +
-      action[randomAction] +
-      " " +
-      what[randomWhat] +
-      " " +
-      when[randomWhen]
-    );
+    return (`${who[WhoRandomIndex]} ${action[ActionRandomIndex]} ${what[WhatRandomIndex]} ${when[WhenRandomIndex]}`);
   }
 });
